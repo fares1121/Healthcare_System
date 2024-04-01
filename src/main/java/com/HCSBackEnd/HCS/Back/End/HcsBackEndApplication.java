774d6650ce3f952a383a23a27352ee529a0014c1
sina-nabavi <sina.nabavi16@gmail.com>
2024-04-01 13:28:46 -0400

    add componentScan for security

--- src/main/java/com/HCSBackEnd/HCS/Back/End/HcsBackEndApplication.java
+++ src/main/java/com/HCSBackEnd/HCS/Back/End/HcsBackEndApplication.java
@@ -2,13 +2,14 @@ package com.HCSBackEnd.HCS.Back.End;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.HCSBackend.HCS.Back.End.security.mappers")
 public class HcsBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HcsBackEndApplication.class, args);
 	}
-
 
 }
